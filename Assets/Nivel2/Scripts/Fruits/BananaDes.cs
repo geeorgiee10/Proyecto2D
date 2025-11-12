e797afcc61f94605d223cfb2e1f47ca071fb91a9
Jorge Arcoya López <georgiearclopez@gmail.com>
2025-11-12 12:40:03 +0100

    frutas nivel 2

---
+++ Assets/Nivel2/Scripts/Fruits/BananaDes.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class BananaDes : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+        transform.GetChild(1).gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+
+            transform.GetChild(1).gameObject.SetActive(true);
+
+            animator.SetTrigger("Desbanana");
+
+            transform.gameObject.SetActive(false);
+
+
+        }
+    }
+}
